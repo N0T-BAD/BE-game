5ff87f32711ab95c4c1a17379bb6baf78eb3f22b
tvfxq0214@gmail.com <tvfxq0214@gmail.com>
2023-05-06 04:39:53 +0900

    feat: 게임 실행 API 생성

--- game-service/src/main/java/com/blockpage/gameservice/adaptor/web/controller/GameController.java
+++ game-service/src/main/java/com/blockpage/gameservice/adaptor/web/controller/GameController.java
@@ -1,12 +1,15 @@
 package com.blockpage.gameservice.adaptor.web.controller;
 
+import com.blockpage.gameservice.adaptor.infrastructure.entity.GameEntity;
 import com.blockpage.gameservice.adaptor.web.apispec.APIResponse;
 import com.blockpage.gameservice.adaptor.web.view.GameView;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +22,38 @@ public class GameController {
     public ResponseEntity<APIResponse> getGame() {
         GameView gameView = new GameView(2, 2, true);
         return ResponseEntity.ok().body(new APIResponse(gameView));
+    }
+
+    @PutMapping
+    public ResponseEntity<APIResponse> updateGame(@RequestParam("type") String type) {
+        switch (type) {
+            case "rullet": {
+                GameEntity gameEntity = GameEntity.builder()
+                    .lottoDayCount(3)
+                    .rulletDayCount(2)
+                    .attendance(false)
+                    .build();
+                return ResponseEntity.ok().body(new APIResponse("룰렛 게임을 실행하였습니다", gameEntity.getRulletDayCount()));
+            }
+            case "lotto": {
+                GameEntity gameEntity = GameEntity.builder()
+                    .lottoDayCount(2)
+                    .rulletDayCount(3)
+                    .attendance(false)
+                    .build();
+                return ResponseEntity.ok().body(new APIResponse("복권 게임을 실행하였습니다", gameEntity.getLottoDayCount()));
+            }
+            case "checks": {
+                GameEntity gameEntity = GameEntity.builder()
+                    .lottoDayCount(3)
+                    .rulletDayCount(3)
+                    .attendance(true)
+                    .build();
+                return ResponseEntity.ok().body(new APIResponse("복권 게임을 실행하였습니다", gameEntity.getAttendance()));
+            }
+            default: {
+                return ResponseEntity.ok().body(new APIResponse("이벤트 실행해보세요"));
+            }
+        }
     }
 }
